c64fbf16eb2b26edd12df65e326a3cd4e0f3b4e9
MarjorieMeunier <61453391+MarjorieMeunier@users.noreply.github.com>
2021-01-07 20:19:57 +0100

    Added header and navbar in home with thymeleaf

--- src/main/java/com/example/emodule/Repository/ChantierRepository.java
+++ src/main/java/com/example/emodule/Repository/ChantierRepository.java
@@ -2,10 +2,11 @@ package com.example.emodule.Repository;
 
 import com.example.emodule.model.Chantier;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@EnableJpaRepositories
 public interface ChantierRepository extends CrudRepository<Chantier, Long> {
 
 }
